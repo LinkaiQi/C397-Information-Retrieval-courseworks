0f46691f04e047afcbe6efce52424ed81a382c80
301githublinkai <linkai@ualberta.ca>
2018-03-23 14:47:07 -0600

    Implement 'create_lms'. Reuse code from previous position index util file

---
+++ assignment2/create_lms.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+import sys
+from os import listdir
+from os.path import join, isfile
+
+from nltk.tokenize import wordpunct_tokenize
+
+from util import send_stdout, progress
+from util import LM_NAME, STEMMER, Token_Preprocessing_Engine
+
+# Global variable for store the LM
+# KEY = docID, VALUE = terms_MLE
+LM_LMS = {}
+
+def read_file(path, docID):
+    # extract text from the path, and tokenize
+    f = open(path, 'r')
+    tokens = wordpunct_tokenize(f.read())
+    f.close()
+
+    # store term frequence for each term occurs in the document d
+    TF = {}
+    # the number of tokens in document d
+    L = len(tokens)
+
+    # Iterate through each term in document d,
+    # use 'TF' accumulator to count the term frequency
+    for token in tokens:
+        # Stemming and Lowercasing
+        if STEMMER:
+            term = st.process_token(token)
+        else:
+            term = token.lower()
+        if term not in TF:
+            TF[term] = 0
+        TF[term] += 1
+
+    TF['L_TOKEN'] = L
+    # Store TF to Global variable LM with its docID
+    LM_LMS[docID] = TF
+
+# filename validation
+# filename format "doc_[docID]_[...]"
+def filename_validation(fname):
+    finfo = fname.split(sep='_', maxsplit=2)
+    if finfo[0] != 'doc':
+        return False, None
+    try:
+        docID = int(finfo[1])
+    except ValueError:
+        return False, None
+    return True, docID
+
+# Main function
+def main():
+    global st
+
+    # read arguments "% ./create_lms [document dir] [output_dir]"
+    if len(sys.argv) != 3:
+        send_stdout("Usage: python3 {} [document dir] [output_dir]".format(sys.argv[0]));
+        return
+    # get filenames from the [document dir]
+    try:
+        DOC_DIR = sys.argv[1]
+        docs = [f for f in listdir(DOC_DIR) if isfile(join(DOC_DIR, f))]
+    except FileNotFoundError as e:
+        send_stdout('Error! No such file or directory "{}".'.format(DOC_DIR))
+        return
+    # check whether the index file already exist in the [output_dir]
+    LM_FILE = join(sys.argv[2], LM_NAME)
+    if isfile(LM_FILE):
+        send_stdout('Error! LM file "{}" already exist.'.format(LM_FILE))
+        return
+
+    # initialize stemmer (Lemmatizer)
+    if STEMMER:
+        st = Token_Preprocessing_Engine()
+
+    skipped_docs = []
+    invalid_filename_docs = []
+    f_num = len(docs);
+    for i in range(f_num):
+        fname = docs[i]
+        success, docID = filename_validation(fname)
+        if not success:
+            invalid_filename_docs.append(fname); continue
+        try:
+            # read file, and create language model (calculate MLE)
+            read_file(join(DOC_DIR, fname), docID)
+        except Exception as e:
+            print(e)
+            skipped_docs.append(fname); continue
+        # update progress bar
+        progress(i+1, f_num)
+
+    send_stdout()
+    # show invalid document name/format to stdout
+    if len(invalid_filename_docs) != 0:
+        send_stdout('Warning! Invalid document name format:')
+        send_stdout('{}, Skipped.'.format(invalid_filename_docs))
+    if len(skipped_docs) != 0:
+        send_stdout('Warning! Cannot process the following doc(s):')
+        send_stdout('{}, Skipped.'.format(skipped_docs))
+
+    # write index to file
+    f_out = open(LM_FILE, 'w')
+    f_out.write(str(LM_LMS))
+    f_out.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ assignment2/util.py
@@ -0,0 +1,53 @@
+import sys
+from pyparsing import *
+
+import nltk
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+
+# Index file name
+LM_NAME = "LM_lms.txt"
+
+# Use stemmer
+STEMMER = False
+# Choose stemmer between the 'Wordnet (Lemmatizer)' and the 'Porter'
+USE_LEMMATIZER = True
+
+
+# Word preprocessing stemming engine
+class Token_Preprocessing_Engine(object):
+    def __init__(self):
+        if USE_LEMMATIZER:
+            # WordnetStemmer (Lemmatizer)
+            nltk.download('wordnet')
+            self.engine = WordNetLemmatizer()
+        else:
+            # PorterStemmer
+            self.engine = PorterStemmer()
+
+    def process_token(self, token):
+        if USE_LEMMATIZER:
+            term = self.engine.lemmatize(token)
+        else:
+            term = self.engine.stem(token)
+        return term.lower()
+
+
+# Send msg to stdout
+def send_stdout(msg=''):
+    sys.stdout.write('{}\n'.format(msg))
+    sys.stdout.flush()
+
+
+# Progress Bar Code Reference:
+# https://gist.github.com/vladignatyev/06860ec2040cb497f0f3
+def progress(count, total, status=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('\r[%s] %s%s ...%s' % (bar, percents, '%', status))
+    sys.stdout.flush()
